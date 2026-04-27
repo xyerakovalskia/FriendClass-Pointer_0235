bb5e83b2a7543f04d044c739eaea5f0ad5147271
xyerakovalskia <xyera123987@gmail.com>
2026-04-27 12:40:04 +0700

    friendclasssatu menambahkan class

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};

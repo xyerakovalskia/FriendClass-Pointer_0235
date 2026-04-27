01cb3db4a17109bea51f4be41aecefc5d7d0cd1d
xyerakovalskia <xyera123987@gmail.com>
2026-04-27 12:35:59 +0700

    pointer satu reference

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -11,3 +11,7 @@ class mahasiswa{
 int main (){
     mahasiswa mhs{1}; // Object mhs
     mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();

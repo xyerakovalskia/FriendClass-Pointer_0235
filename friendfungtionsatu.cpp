7969a2f8f8fc0a422a082c38b0208f0f730575cc
xyerakovalskia <xyera123987@gmail.com>
2026-04-27 12:34:38 +0700

    pointer satu object mhs

---
+++ friendfungtionsatu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    string nama;
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
--- pointersatu.cpp
+++ pointersatu.cpp
@@ -7,3 +7,7 @@ class mahasiswa{
         cout<<"No Induk = "<<nim<<endl;
     }
 };
+
+int main (){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //Member Access Operator
